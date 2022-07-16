16be67fdcc1ce0f887ee18a8235f56a94e9b53dd
Alexis <alexis.sousa@giffits.de>
2022-07-16 20:51:34 +0200

    link this to issue number #4

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        now the commit refers the cardinal 
         <a
           className="App-link"
           href="https://reactjs.org"
