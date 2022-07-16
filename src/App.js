2c19b8e0b88af6e916abbf881aaddb9fc4421688
Alexis <alexis.sousa@giffits.de>
2022-07-16 20:57:23 +0200

    #3 this was a commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        I made some change to number three
         <a
           className="App-link"
           href="https://reactjs.org"
